6db1b309a3e23e72594cd0064fadfe2588f52ff2
Piers Storey <piers.storey@gmail.com>
2016-01-03 23:02:19 +0000

    pep8 added and checked against app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 """Flask app for use in Jenkins testing"""
 from flask import Flask, jsonify
 
-
 APP = Flask(__name__)
+
 
 @APP.route('/')
 def favicon():
     """Return message for default route."""
-    return jsonify({'message' : 'Hello pythonistic world'})
+    return jsonify({'message': 'Hello pythonistic world'})
 
 if __name__ == '__main__':
     APP.run(threaded=True)
