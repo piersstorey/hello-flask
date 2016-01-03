bb66ae8038deabe279c202e45d7aefc9408faee7
Piers Storey <piers.storey@gmail.com>
2016-01-03 22:41:41 +0000

    pylint added and app.py checked against violations

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask,  jsonify
+#!flask/bin/python
+"""Flask app for use in Jenkins testing"""
+from flask import Flask, jsonify
 
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-
-# Default hello world route
-@app.route('/')
+@APP.route('/')
 def favicon():
+    """Return message for default route."""
     return jsonify({'message' : 'Hello pythonistic world'})
 
-
 if __name__ == '__main__':
-	app.run(threaded=True)
+    APP.run(threaded=True)
